11f753033cbe57c82adf91064535e6ba8217919a
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-22 13:28:28 +0300

    imported the auth screen to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat/screens/authscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255,63,17,177)),
         useMaterial3: true,
       ),
-      home: ...,
+      home: AuthScreen(),
     );
   }
 }
