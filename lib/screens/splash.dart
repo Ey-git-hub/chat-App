bb00585149ec51eef54d788ccc441fea9b5e1e7e
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-24 11:48:04 +0300

    added the splash screen

---
+++ lib/screens/splash.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatelessWidget{
+  const SplashScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('chatApp'),),
+      body: Center(child: Text('Loading...'),));
+  }
+}
