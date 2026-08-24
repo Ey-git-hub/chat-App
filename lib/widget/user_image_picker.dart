ea3df64a747d3cfdcde74711bd22e96a4b61ece0
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-24 20:00:37 +0300

    added the method to pick image

--- lib/widget/user_image_picker.dart
+++ lib/widget/user_image_picker.dart
@@ -1,5 +1,8 @@
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImage extends StatefulWidget{
   const UserImage({super.key});
@@ -10,9 +13,29 @@ class UserImage extends StatefulWidget{
 
 }
 class _UserImageState extends State<UserImage>{
+  File? _pickedImageFile;
+
+  void _pickImage() async{
+    final pickedImage=await ImagePicker().pickImage(source: ImageSource.camera,maxHeight: 150,imageQuality: 50);
+    if(pickedImage==null){
+      return;
+    }
+    setState(() {
+      _pickedImageFile=File(pickedImage.path);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-   return ;
+   return Column(children: [
+    CircleAvatar(
+      radius: 40,
+      backgroundColor: Colors.grey,
+      foregroundImage: _pickedImageFile !=null ? FileImage(_pickedImageFile!):null,
+    ),
+    TextButton.icon(onPressed: _pickImage,icon: Icon(Icons.image) , label: Text("Add Image",style: TextStyle(
+      color:Theme.of(context).colorScheme.primary
+    ),))
+   ],);
   }
 
 }
