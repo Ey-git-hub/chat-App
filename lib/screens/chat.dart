ac1aa5f4a6006135fcfa2c6b3c8c289414787854
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-24 12:31:36 +0300

    added the logout logic

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,7 +8,13 @@ class ChatScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('chatApp'),),
+      appBar: AppBar(title: Text('chatApp'),
+      actions: [
+        IconButton(onPressed: (){
+          FirebaseAuth.instance.signOut();
+        }, icon: Icon(Icons.exit_to_app),color:Theme.of(context).colorScheme.primary)
+       
+      ],),
       body: Center(child: Text('Chat Screen(Logged in!) '),));
   }
 }
