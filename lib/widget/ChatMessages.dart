5028b1828d8beb8d365d00d72c0d31d9fe7f2349
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-25 12:24:58 +0300

    added the chat messages widget

---
+++ lib/widget/ChatMessages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget{
+  ChatMessages({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+       child: Text("No text messgaes"),
+    );
+  }
+}
