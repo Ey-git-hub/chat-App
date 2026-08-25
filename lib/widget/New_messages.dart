da6b2c989403709d22a217727d0e8842703c5da1
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-08-25 12:58:26 +0300

    acessing user data

--- lib/widget/New_messages.dart
+++ lib/widget/New_messages.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessages extends StatefulWidget{
@@ -16,18 +18,31 @@ _messageController.dispose();
   super.dispose();
 }
 
-  void _submitMessage(){
+  void _submitMessage() async{
   final _enterdtext=_messageController.text;
   if (_enterdtext.trim().isEmpty){
     return;
   }
-//send to fire base
+      FocusScope.of(context).unfocus();
     _messageController.clear();
+
+//send to fire base
+final user= await FirebaseAuth.instance.currentUser!;
+final userData= await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+
+
+FirebaseFirestore.instance.collection('chat').add({
+  'text':_enterdtext,
+  'createdAt':Timestamp.now(),
+  'userid':user.uid,
+  'username':userData.data()!['username'],
+  'userImage':userData.data()!['image_data'],
+});
 
   }
   @override
   Widget build(BuildContext context) {
-   return Padding(padding: EdgeInsets.only(right:1,top:2,left:20,bottom: 50),
+   return Padding(padding: EdgeInsets.only(right:1,top:2,left:20,bottom: 40),
    child: Row(children: [
     Expanded(child: TextField(
       controller: _messageController,
